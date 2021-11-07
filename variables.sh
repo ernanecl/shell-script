a8f59f803f05f4aeebec0cf28df953de5ea03404
ernanecl <ernanecl@gmail.com>
2021-11-07 15:17:16 -0300

    renaming shell.sh to mentoria.sh and creating a new file named variables.sh

---
+++ variables.sh
@@ -0,0 +1,10 @@
+'
+em shell precisamos inserir qual vai ser o seu compilador
+
+caminho absoluto:
+    #!/bin/bash
+
+**não é recomendado manter o caminho absoluto, outras distribuições são caminhos diferentes
+'
+#!/usr/bin/env bash
+
