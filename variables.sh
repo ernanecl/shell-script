e2519a02106aba7daf0b314c37048679856a171b
ernanecl <ernanecl@gmail.com>
2021-11-07 15:20:40 -0300

    adding comments in variable.sh file

--- variables.sh
+++ variables.sh
@@ -3,8 +3,23 @@ em shell precisamos inserir qual vai ser o seu compilador
 
 caminho absoluto:
     #!/bin/bash
-
 **não é recomendado manter o caminho absoluto, outras distribuições são caminhos diferentes
+
+recomendado usar o caminho:
+    #!/usr/bin/env bash
+nesse caso, o "env" busca o bash dentro das variáveis de ambiente
 '
 #!/usr/bin/env bash
 
+'
+ver permissões de arquivos e diretórios:
+    ll
+    ls -l
+    ls -la
+
+para dar permissão:
+    chmod +x file_name.sh
+
+para executar o arquivo shell:
+    ./file_name.sh
+'
